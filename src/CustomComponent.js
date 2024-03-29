8d4115f7519f6f722fcdbeea50a08b46d4bcbc67
rohit sharma <rohitjsarma@gmail.com>
2024-03-29 16:53:22 +0530

    addedCustopmComp

--- src/CustomComponent.js
+++ src/CustomComponent.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const CustomComponent = ({ label }) => {
   return (
-    <div>
+    <div style={{ width: '100%', height: '100%' }}>
       <label>{label}</label>
-      <input type="text" />
+    
+      <input type="text" style={{ width: '100%', height: '100%', border: 'none', outline: 'none', background: 'transparent' }} />
     </div>
   );
 };
--- src/FormLayout.js
+++ src/FormLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { WidthProvider, Responsive } from 'react-grid-layout';
-import { Resizable } from 'react-resizable'; // Import Resizable from react-resizable
+import { Resizable } from 'react-resizable'; 
+import CustomComponent from './CustomComponent'; 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
@@ -15,6 +16,7 @@ const FormLayout = () => {
 
   return (
     <ResponsiveReactGridLayout className='layout' layouts={{ lg: layout }}>
+      {/* Replace the first field with CustomComponent */}
       <div key='a' data-grid={{ x: 0, y: 0, w: 2, h: 2 }}>
         <Resizable
           width={200}
@@ -22,10 +24,11 @@ const FormLayout = () => {
           minConstraints={[100, 30]}
         >
           <div style={{ width: '100%', height: '100%', border: '1px solid #ccc', borderRadius: '5px', padding: '5px' }}>
-            <input type='text' placeholder='Enter text' style={{ width: '100%', height: '100%', border: 'none', outline: 'none', background: 'transparent' }} />
+            <CustomComponent label='Custom Field' />
           </div>
         </Resizable>
       </div>
+     
       <div key='b' data-grid={{ x: 2, y: 0, w: 2, h: 2 }}>
         <Resizable
           width={200}
